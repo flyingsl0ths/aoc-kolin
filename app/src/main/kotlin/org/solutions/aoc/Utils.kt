3319b29537a74c6b9858ab487756dbf4d3358e0d
flyingsl0ths <randomemail33110@protonmail.com>
2022-12-01 16:56:22 -0600

    Remove unecessary use

--- app/src/main/kotlin/org/solutions/aoc/Utils.kt
+++ app/src/main/kotlin/org/solutions/aoc/Utils.kt
@@ -3,20 +3,18 @@ import kotlin.reflect.KClass
 inline fun <T : Any> readResource(
                 clss: KClass<T>,
                 path: String,
-                noinline predicate: (String) -> Boolean,
+                crossinline predicate: (String) -> Boolean,
                 crossinline onLine: (String) -> Unit,
                 crossinline onNotLine: (String) -> Unit
 ): Unit {
         val file = clss.java.classLoader.getResourceAsStream(path)
 
-        file.use {
-                it.bufferedReader().use {
-                        it.lines().forEach {
-                                if (predicate(it)) {
-                                        onLine(it)
-                                } else {
-                                        onNotLine(it)
-                                }
+        file.bufferedReader().use {
+                it.lines().forEach {
+                        if (predicate(it)) {
+                                onLine(it)
+                        } else {
+                                onNotLine(it)
                         }
                 }
         }
