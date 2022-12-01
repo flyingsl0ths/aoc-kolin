945c2d4e37a217c17703f0d470846118346d409b
flyingsl0ths <randomemail33110@protonmail.com>
2022-12-01 16:11:42 -0600

    Implement Day 1 solutions

--- app/src/main/kotlin/org/solutions/aoc/App.kt
+++ app/src/main/kotlin/org/solutions/aoc/App.kt
@@ -1,5 +1,11 @@
 package org.solutions.aoc
 
+import org.solutions.aoc.days.DayOne
+
 fun main() {
-    println("Hello world!!")
+    val (id, calories) = DayOne.one()
+    println("Elf $id: $calories")
+
+    val topThreeSum = DayOne.two()
+    println("Top 3 sum: $topThreeSum")
 }
---
+++ app/src/main/kotlin/org/solutions/aoc/days/One.kt
@@ -0,0 +1,54 @@
+package org.solutions.aoc.days
+
+import readResource
+
+typealias CalorieReport = Pair<Int, Int>
+
+class CaloriesReport(var maxCalories: Int = 0, var calorieCount: Int = 0, var id: Int = 0) {
+        fun result(): CalorieReport = this.id to this.maxCalories
+}
+
+inline fun withInput(
+                crossinline pred: (String) -> Boolean,
+                crossinline onLine: (String) -> Unit,
+                crossinline onNotLine: (String) -> Unit
+): Unit {
+        readResource(DayOne::class, "./day/1.txt", pred, onLine, onNotLine)
+}
+
+final class DayOne constructor() {
+        companion object {
+                fun one(): CalorieReport {
+                        val ctx = CaloriesReport()
+
+                        val pred = { line: String -> line.isEmpty() }
+
+                        val onLine = { _: String ->
+                                ctx.id += 1
+                                ctx.maxCalories = Math.max(ctx.maxCalories, ctx.calorieCount)
+                                ctx.calorieCount = 0
+                        }
+
+                        withInput(pred, onLine) { line: String -> ctx.calorieCount += line.toInt() }
+
+                        return ctx.result()
+                }
+
+                fun two(): Int {
+                        val ctx = mutableListOf<Int>()
+
+                        var sum = 0
+
+                        val pred = { line: String -> line.isEmpty() }
+
+                        val onLine = { _: String ->
+                                ctx.add(sum)
+                                sum = 0
+                        }
+
+                        withInput(pred, onLine) { line: String -> sum += line.toInt() }
+
+                        return ctx.sortedDescending().take(3).sum()
+                }
+        }
+}
