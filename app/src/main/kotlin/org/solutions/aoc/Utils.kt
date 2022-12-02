26f243e4fdb94701bd77763efe62444689597e0d
flyingsl0ths <randomemail33110@protonmail.com>
2022-12-01 22:16:51 -0600

    Refactor Utils::readResource

--- app/src/main/kotlin/org/solutions/aoc/Utils.kt
+++ app/src/main/kotlin/org/solutions/aoc/Utils.kt
@@ -1,21 +1,24 @@
 import kotlin.reflect.KClass
 
-inline fun <T : Any> readResource(
+inline fun <T : Any, U> readResource(
                 clss: KClass<T>,
                 path: String,
+                ctx: U,
                 crossinline predicate: (String) -> Boolean,
-                crossinline onLine: (String) -> Unit,
-                crossinline onNotLine: (String) -> Unit
-): Unit {
+                crossinline onLine: (U, String) -> U,
+                crossinline onNotLine: (U, String) -> U
+): U {
         val file = clss.java.classLoader.getResourceAsStream(path)
+        var acc = ctx
 
         file.bufferedReader().use {
                 it.lines().forEach {
                         if (predicate(it)) {
-                                onLine(it)
+                                acc = onLine(acc, it)
                         } else {
-                                onNotLine(it)
+                                acc = onNotLine(acc, it)
                         }
                 }
         }
+        return acc
 }
--- app/src/main/kotlin/org/solutions/aoc/days/One.kt
+++ app/src/main/kotlin/org/solutions/aoc/days/One.kt
@@ -8,47 +8,57 @@ final class CaloriesReport(var maxCalories: Int = 0, var calorieCount: Int = 0, 
         inline fun result(): CalorieReport = this.id to this.maxCalories
 }
 
-inline fun withInput(
-                crossinline pred: (String) -> Boolean,
-                crossinline onLine: (String) -> Unit,
-                crossinline onNotLine: (String) -> Unit
-): Unit {
-        readResource(DayOne::class, "./day/1.txt", pred, onLine, onNotLine)
+inline fun <T> withInput(
+                ctx: T,
+                crossinline onLine: (T, String) -> T,
+                crossinline onNotLine: (T, String) -> T
+): T {
+        return readResource<DayOne, T>(
+                        DayOne::class,
+                        "./day/1.txt",
+                        ctx,
+                        { line: String -> line.isEmpty() },
+                        onLine,
+                        onNotLine
+        )
 }
 
 final class DayOne constructor() {
         companion object {
                 fun one(): CalorieReport {
-                        val ctx = CaloriesReport()
-
-                        val pred = { line: String -> line.isEmpty() }
-
-                        val onLine = { _: String ->
+                        val onLine = { ctx: CaloriesReport, _: String ->
                                 ctx.id += 1
                                 ctx.maxCalories = Math.max(ctx.maxCalories, ctx.calorieCount)
                                 ctx.calorieCount = 0
+                                ctx
                         }
 
-                        withInput(pred, onLine) { line: String -> ctx.calorieCount += line.toInt() }
+                        val acc =
+                                        withInput<CaloriesReport>(CaloriesReport(), onLine) {
+                                                        ctx: CaloriesReport,
+                                                        line: String ->
+                                                ctx.calorieCount += line.toInt()
+                                                ctx
+                                        }
 
-                        return ctx.result()
+                        return acc.result()
                 }
 
                 fun two(): Int {
-                        val ctx = mutableListOf<Int>()
-
-                        var sum = 0
-
-                        val pred = { line: String -> line.isEmpty() }
-
-                        val onLine = { _: String ->
-                                ctx.add(sum)
-                                sum = 0
+                        val onLine = { ctx: Pair<MutableList<Int>, Int>, _: String ->
+                                ctx.first.add(ctx.second)
+                                ctx.first to 0
                         }
 
-                        withInput(pred, onLine) { line: String -> sum += line.toInt() }
+                        val (results, _) =
+                                        withInput((mutableListOf<Int>() to 0), onLine) {
+                                                        ctx: Pair<MutableList<Int>, Int>,
+                                                        line: String ->
+                                                ctx.first to (ctx.second + line.toInt())
+                                        }
 
-                        return ctx.sortedDescending().take(3).sum()
+                        results.sortDescending()
+                        return results.take(3).sum()
                 }
         }
 }
